75f0325df008f7e34fd88ec8040d0c1a87c5541d
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-11-10 23:49:11 -0300

    feat(posts): 🚧 show dynamic content on /post/home
    delete hello world

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+  {"title": "Deserunt est et qui occaecat nisi non ullamco officia.",
+   "content": "Ut eiusmod laborum culpa proident minim culpa amet esse ut culpa tempor veniam. Consequat quis occaecat voluptate laboris adipisicing nulla consequat sint ipsum. In officia amet consectetur ipsum do irure laborum labore do sint cillum amet."},
+  {"title": "Ullamco est duis consequat mollit voluptate aliqua.",
+   "content": "Est irure sint non duis fugiat cupidatat aute enim. Dolor tempor laboris ad mollit nulla exercitation cillum elit. Commodo ea aliquip Lorem dolore dolore minim. Ullamco culpa duis ad reprehenderit labore."},
+  {"title": "Nisi pariatur nostrud amet fugiat ea.",
+   "content": "Proident ad nisi ipsum reprehenderit ullamco excepteur cupidatat esse ex in. Cillum velit voluptate do laboris commodo ullamco minim ipsum aliquip nostrud commodo tempor laborum. Tempor do voluptate et nostrud sint non nisi. Elit nulla ipsum sint ea culpa duis. Ipsum irure sunt laboris est id enim est ea commodo id. Cillum esse voluptate ipsum culpa exercitation nostrud."}
+]
+
 # Create your views here.
-def hello_world(request):
-  return HttpResponse("Hello World!")
+def home(request):
+  html = ""
+  for post in posts:
+    html += f"""
+      <h2>{post["title"]}</h2>
+      <p>{post["content"]}</p> 
+    """
+  return HttpResponse(html)
